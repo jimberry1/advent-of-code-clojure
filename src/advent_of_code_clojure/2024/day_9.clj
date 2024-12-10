318953d2c0744b2990665023409357ec3742b52e
jim.berry <jim.berry@signal-ai.com>
2024-12-10 19:02:34 +0000

    AoC day 9 further optimisations

--- src/advent_of_code_clojure/2024/day_9.clj
+++ src/advent_of_code_clojure/2024/day_9.clj
@@ -7,14 +7,14 @@
 (defn prepare-input [input]
   (utils/format-input input :line-re-split #"" :val-xform utils/->int :input-xform first))
 
-(defn expand-list [input]
+(defn expand-list-p1 [input]
   (flatten (map-indexed (fn [idx file-size]
                           (if (= (rem idx 2) 0)
                             (take file-size (repeat (/ idx 2)))
                             (take file-size (repeat "."))))
                         input)))
 
-(defn condense-list [expanded-list]
+(defn reorganise-files-p1 [expanded-list]
   (loop [[head & rest-list] expanded-list from (reverse expanded-list) res []]
     (cond
       (nil? head) res
@@ -25,8 +25,8 @@
 (defn solve [input]
   (->> input
        prepare-input
-       expand-list
-       condense-list
+       expand-list-p1
+       reorganise-files-p1
        (take-while (partial not= "."))
        (map-indexed (fn [idx item]
                       (* idx item)))
@@ -34,21 +34,26 @@
 
 (time (solve input))
 
-(defn p2-expand [items]
+(defn create-or-insert-nested-set [map key space-pos]
+  (if (contains? map key)
+    (update map key conj space-pos)
+    (assoc map key (sorted-set space-pos))))
+
+(defn analyse-file-structure [items]
   (reduce (fn [[file-map space-map current-pos index] next-item]
             (if (= (rem index 2) 0)
               (let [updated-file-map (assoc file-map current-pos [(/ index 2) next-item])]
                 [updated-file-map space-map (+ current-pos next-item) (inc index)])
-              (let [updated-space-map (assoc space-map current-pos next-item)]
+              (let [updated-space-map (create-or-insert-nested-set space-map next-item current-pos)]
                 [file-map updated-space-map (+ current-pos next-item) (inc index)])))
           [(sorted-map) (sorted-map) 0 0] items))
 
-(defn find-space [space-map file-size file-pos]
-  (reduce (fn [_acc [space-pos space-size :as space]]
-            (cond
-              (> space-pos file-pos) (reduced nil)
-              (<= file-size space-size) (reduced space)
-              :else nil)) nil space-map))
+(defn find-available-space [space-map file-size file-pos]
+  (->> space-map
+       (keep (fn [[space-size space-positions]]
+               (when (and (<= file-size space-size) (< (first space-positions) file-pos)) [(first space-positions) space-size])))
+       (sort-by first)
+       first))
 
 (defn update-file-map [file-map space-pos file-pos file]
   (-> file-map
@@ -56,27 +61,27 @@
       (dissoc file-pos)))
 
 (defn update-space-map [space-map [space-pos space-size] file-pos file-size]
-  (cond-> space-map
-    :always (dissoc space-pos)
-    :always (assoc file-pos file-size)
-    (< file-size space-size) (assoc (+ space-pos file-size) (- space-size file-size))))
+  (let [new-file-size (- space-size file-size)]
+    (cond-> space-map
+      :always (update space-size disj space-pos)
+      :always (create-or-insert-nested-set file-size file-pos)
+      (not= 0 new-file-size) (create-or-insert-nested-set new-file-size (+ space-pos file-size)))))
 
 (defn loop-file [initial-file-map initial-space-map]
   (loop [[[file-pos [_file-id file-size :as next-file]] & rem] (reverse initial-file-map) space-map initial-space-map res initial-file-map]
     (if (nil? next-file)
       res
-      (let [[space-pos :as space] (find-space space-map file-size file-pos)]
-        (if (nil? space-pos)
-          (recur rem space-map res)
-          (let [updated-res (update-file-map res space-pos file-pos next-file)
-                updated-space-map (update-space-map space-map space file-pos file-size)]
-            (recur rem updated-space-map updated-res)))))))
+      (if-let [[space-pos :as space] (find-available-space space-map file-size file-pos)]
+        (recur rem
+               (update-space-map space-map space file-pos file-size)
+               (update-file-map res space-pos file-pos next-file))
+        (recur rem space-map res)))))
 
-(defn solve-better [input]
-  (let [[initial-file-map initial-space-map] (->> input prepare-input p2-expand)
+(defn solve-p2 [input]
+  (let [[initial-file-map initial-space-map] (->> input prepare-input analyse-file-structure)
         condensed-file (loop-file initial-file-map initial-space-map)]
     (->> condensed-file
          (transduce (mapcat (fn [[idx [file-id size]]]
                               (map #(* % file-id) (range idx (+ idx size))))) +))))
 
-(time (solve-better input))
+(time (solve-p2 input))
